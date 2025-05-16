66e36e510ca3f171a68f4d59312e31c2d0c5aa42
priyanshu <priyanshoe.official@gmail.com>
2025-05-16 19:02:21 +0530

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/homepage",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
